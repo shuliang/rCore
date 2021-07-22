e30be0c38f61e27faf03b962650a7f2219223f3f
ShuLiang <shuliang@live.cn>
2021-07-22 23:40:49 +0800

    Refactoring

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,75 +4,12 @@
 #![feature(global_asm)]
 #![feature(panic_info_message)]
 
-use core::fmt::{self, Write};
-use core::panic::PanicInfo;
-
-// mod lang_items;
+#[macro_use]
+mod console;
+mod lang_items;
 mod sbi;
 
 global_asm!(include_str!("entry.asm"));
-
-const SYSCALL_EXIT: usize = 93;
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        llvm_asm!("ecall"
-            : "={x10}" (ret)
-            : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
-            : "memory"
-            : "volatile"
-        );
-    }
-    ret
-}
-
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-const SYSCALL_CONSOLE_PUTCHAR: usize = 1;
-
-pub fn console_putchar(c: usize) {
-    syscall(SYSCALL_CONSOLE_PUTCHAR, [c, 0, 0]);
-}
-
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        // for c in s.chars() {
-        for c in s.bytes() {
-            console_putchar(c as usize);
-        }
-        Ok(())
-    }
-}
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
-}
-
-const SBI_SHUTDOWN: usize = 8;
-
-pub fn shutdown() -> ! {
-    sbi::sbi_call(SBI_SHUTDOWN, 0, 0, 0);
-    panic!("It should shutdown!");
-}
 
 fn clean_bss() {
     extern "C" {
@@ -82,27 +19,10 @@ fn clean_bss() {
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    if let Some(location) = info.location() {
-        println!(
-            "Panicked at {}:{} {}",
-            location.file(),
-            location.line(),
-            info.message().unwrap()
-        );
-    } else {
-        println!("Panicked: {}", info.message().unwrap());
-    }
-    shutdown()
-}
-
 #[no_mangle]
 pub fn rust_main() -> ! {
     clean_bss();
     println!("Hello, world!");
     println!("Powered by Rust with ♥");
     panic!("It should shutdown!");
-    // sys_exit(9);
-    // shutdown();
 }
