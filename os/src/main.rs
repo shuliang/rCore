124cd129f7ea0fb020cea4910f2154ad334b0393
ShuLiang <shuliang@live.cn>
2021-07-22 22:21:17 +0800

    Add user-mode print

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,6 +2,8 @@
 #![no_main]
 #![feature(llvm_asm)]
 
+use core::fmt::{self, Write};
+
 mod lang_items;
 
 const SYSCALL_EXIT: usize = 93;
@@ -23,7 +25,42 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
+const SYSCALL_WRITE: usize = 64;
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
 #[no_mangle]
 extern "C" fn _start() {
+    println!("Hello, world!");
+    println!("Powered by Rust with ♥");
     sys_exit(9);
 }
