06c9318bf4bf32543d95ad2943fba34d21b944f1
ShuLiang <shuliang@live.cn>
2021-07-23 16:46:14 +0800

    Add color log

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,7 +4,11 @@
 #![feature(global_asm)]
 #![feature(panic_info_message)]
 
+use log::{debug, error, info, trace, warn};
+
 #[macro_use]
+mod logging;
+
 mod console;
 mod lang_items;
 mod sbi;
@@ -21,8 +25,36 @@ fn clean_bss() {
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
     clean_bss();
+    logging::init();
     println!("Hello, world!");
+    error!("This is a error msg.");
+    warn!("This is a warn msg.");
+    info!("This is a info msg.");
+    debug!("This is a debug mesg.");
+    trace!("This is a trace msg.");
+
     println!("Powered by Rust with ♥");
+    error!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    warn!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    debug!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    trace!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
     panic!("It should shutdown!");
 }
