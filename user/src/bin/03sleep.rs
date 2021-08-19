7536659943681ecfe5a9f63534c87fe66359f7d8
ShuLiang <shuliang@live.cn>
2021-08-19 16:55:02 +0800

    User apps changes corresponding to introduce virtual memory

--- user/src/bin/03sleep.rs
+++ user/src/bin/03sleep.rs
@@ -8,8 +8,8 @@ use user_lib::{get_time, yield_};
 
 #[no_mangle]
 fn main() -> i32 {
-    let current_time = get_time();
-    let wait_for = current_time + 3000;
+    let current_timer = get_time();
+    let wait_for = current_timer + 3000;
     while get_time() < wait_for {
         yield_();
     }
